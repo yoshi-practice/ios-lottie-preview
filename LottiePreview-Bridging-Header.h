f192d6dd7f036669c3eed9fdfb6cde2d300b0ca6
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-06 21:25:30 +0900

    [Update] First commit

---
+++ LottiePreview-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  
+//
+//  Created by Yoshihisa Kaino on 2018/12/06.
+//
+
+#ifndef LottiePreview_Bridging_Header_h
+#define LottiePreview_Bridging_Header_h
+
+#import <Lottie/Lottie.h>
+
+#endif /* LottiePreview_Bridging_Header_h */
--- LottiePreviewSample/ViewController.swift
+++ LottiePreviewSample/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black // 背景色を黒に
+        let animationView = LOTAnimationView(name: "worldlocations")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        animationView.center = self.view.center
+        animationView.loopAnimation = true
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1
+        
+        view.addSubview(animationView)
+        
+        animationView.play()
     }
 
 
