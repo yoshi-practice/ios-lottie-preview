e16456f1b7352a4346e7b3427e35d54f4b348f1c
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-06 20:54:32 +0900

    Initial Commit

---
+++ LottiePreviewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottiePreviewSample
+//
+//  Created by Yoshihisa Kaino on 2018/12/06.
+//  Copyright © 2018 Yoshihisa Kaino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
